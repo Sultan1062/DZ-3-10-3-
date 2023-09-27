edad8c8b491705755b7ba60577634c53de925ffb
sulta <sulta@DESKTOP-E785JP2>
2023-09-27 11:47:49 +0300

    Добавьте файлы проекта.

---
+++ dz6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int value;
+            int i = 2;
+            bool simple = false;
+
+            while (true)
+            {
+                Console.WriteLine("проверка числа:");
+                value = Convert.ToInt32(Console.ReadLine());
+
+                if ((value % i == 0)(i <= value - 1))
+                {
+                    i++;
+                    Console.WriteLine("число не простое");
+                    simple = true;
+                    Console.WriteLine(simple);
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("число простое");
+                    Console.WriteLine(simple);
+                    Console.ReadKey();
+                }
+                    break;
+                }
+                
+            }
+        }
+    }
+}
